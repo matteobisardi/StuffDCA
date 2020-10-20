795d0fe1bcd76724f0e16d2aa375f56901c63a43
matteobisardi <63350069+matteobisardi@users.noreply.github.com>
2020-10-20 12:17:05 +0200

    Update read_write.jl

--- src/read_write.jl
+++ src/read_write.jl
@@ -107,3 +107,49 @@ function map_gaps(matrix)
 	end
 	return matrix
 end
+
+
+
+export read_par_BM
+
+"""
+	read_par_BM(path::AbstractString, q, N)
+
+	Reads the parameters of a Potts model in format
+	
+	J i j a b
+	...
+	h i a 
+
+	and returns them in tensor format.
+	J[a, b, i, j] is such that  1 == "A", ... "21" == "-",
+	and the same for  h[a, i].
+
+"""
+
+function read_par_BM(path::AbstractString, q, N)
+	data = readdlm(path,' ', use_mmap = true)[:, 2:6]
+	J = Array{Float64}(undef, q, q, N, N)
+	h = Array{Float64}(undef, q, N)
+	n_J = Int(q*q*N*(N-1)/2)
+	n_h = q*N
+
+	for k in 1:n_J
+		i, j, a, b, par_j = data[k, :]
+		i += 1
+		j += 1
+		a == 0 && (a = 21)
+		b == 0 && (b = 21)
+		J[a, b, i, j] = par_j
+	end
+
+	for l in (n_J + 1): n_h + n_J
+		i, a, par_h = data[l, :]
+		i += 1
+		a == 0 && (a = 21)
+		h[a, i] = par_h
+	end
+
+	return h, J
+end
+
