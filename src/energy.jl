b35e49c6b389da4fa1cdfda2c2140af58f0a79ac
matteobisardi <63350069+matteobisardi@users.noreply.github.com>
2020-10-20 12:15:28 +0200

    Update energy.jl

--- src/energy.jl
+++ src/energy.jl
@@ -1,1 +1,21 @@
 
+export compute_energy_single_sequence
+
+"""
+
+"""
+function compute_energy_single_sequence(h::Array{Float64,2},
+                                        J::Array{Float64,4},
+                                        S::Vector)
+    N = size(h)[2]
+    q = size(h)[1]
+    E = 0.0
+    for i = 1:N
+        E -= h[S[i],i]
+        for j = (i+1):N
+			E -= J[S[i],S[j],i,j]
+		end
+	end
+return E
+end
+
